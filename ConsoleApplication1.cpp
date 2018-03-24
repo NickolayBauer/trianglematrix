ccf267d97aa9283ba9a81fc9555404180e2cf7e5
Nickolas <35115545+NickolayBauer@users.noreply.github.com>
2018-03-24 10:47:01 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,3 @@
-// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
@@ -25,14 +22,13 @@ int main()
 		}
 	}
 	
-
-	
-
-	int k = 0, s = 0, o = 0,check = 0;
-	for (int i = 0; i < n; i++)
+	int k = 0, s = 0, o = 0,check = 0, equal = 0;
+	for (int i = 0; i < n; i++) 
+	{
+		equal = i;
 		for (int j = 0; j < i; j++)
 		{
-			
+			if (a[i][equal] == 0) check++;
 			if (a[i][j] == 0)s++; //проверяет элементы ниже гл. диагонали 
 			if (a[n - i - 1][n - j - 1] == 0) o++; //проверяет элементы выше гл. диагонали 
 
@@ -40,13 +36,8 @@ int main()
 			k++; //кол-во элементов выше или ниже гл. диагонали (оно одинаковое т.к. матрица квадратная) 
 
 		}
+	}
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) {
-			if ((i == j) && (a[i][j] == 0)) check++;
-		}
-
-	//cout << "s= " << s << " " << "o=" << o << " " << "k=" << k << " " << "check=" << check << endl;
 	if ((s == k) && (o == k) && (check == n)) cout <<"Нулевая матрица" << endl;
 	else if ((s == k) && (o == k)) cout << "Диагональная матрица" << endl;
 	else if (s == k) cout << "Треугольная нижняя" << endl;
